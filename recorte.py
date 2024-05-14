16e8c0f76b913227b7b6e268a2f49c4a8772e09f
Igor Kaiser Gris <igorkagris@hotmail.com>
2024-05-13 22:56:17 -0300

    Final do dia

--- recorte.py
+++ recorte.py
@@ -1,15 +1,29 @@
 
 
+def intersection_calc(P_out, P_in, Xmin, Ymin, Xmax, Ymax):
+    if P_out[0] <= Xmax and P_out[0] >= Xmin:
+        if P_out[1] < Ymin: #Quando o ponto está acima da janela
+
+
+        elif P_out[1] > Ymax: #Ponto está abaixo da janela 
+
+    elif P_out[0] < Xmin: #P na esquerda
+        #Realiza o recorte
+    
+    elif P_out[0] > Xmax: #P na direita
+
+
 
 def recorte(A, Xmin, Ymin, Xmax, Ymax):
-    #L1 = Polígono
-    #L2 = Janela
-    #L3 = Pontos entrando na janela
-    #flag = 0 para dentro da viewport, 1 para fora
-    for p in A:
+    L1 = [] #Polígono
+    L2 = [] #Janela
+    L3 = [] #Pontos entrando na janela
+            #flag = 0 para dentro da viewport, 1 para fora
+    for i in range (len(A)):
+
         if p[0] < Xmin  or p[0] > Xmax or p[1] < Ymin or p[1] > Ymax:
             if flag == 0: #Quando sai da viewport
-                pn = intersection_calc()
+                pn = intersection_calc(A, B, Xmin, Ymin, Xmax, Ymax)
                 #L1.append(p interseção)
                 #L2.append(p interseção)
             flag = 1
@@ -21,5 +35,6 @@ def recorte(A, Xmin, Ymin, Xmax, Ymax):
                 #L2.append(p interseção)
                 #L3.append(p interseção)
             flag = 0
+        L1.append(A[i])
                 
         flag = 0
---
+++ z_buffer.py
@@ -0,0 +1,38 @@
+#Válido apenas para polígonos convexos
+
+def rasterizar_vertices(vertices):
+    ordered_vertices = sorted(vertices, key=lambda v: v[1]) # Ordena o vértices crescente em y
+
+    y_min = ordered_vertices[0][1]
+    y_max = ordered_vertices[-1][1]
+
+
+    bordas_esquerda = []   #Lista para armazenar x para cada y
+    bordas_direita = []
+
+   
+    for i in range(len(ordered_vertices)):      # Calcular as bordas esquerda e direita para cada linha
+
+        x1, y1 = ordered_vertices[i]
+        if (i >= len(ordered_vertices)): 
+            x2, y2 = ordered_vertices[0]
+        x2, y2 = ordered_vertices[i + 1]
+
+        if y1 != y2:  #Linha não horizontal
+            tx = (x2 - x1) / (y2 - y1) # Calcular a inclinação da aresta
+
+            for y in range (y2-y1):  # Percorrer cada linha entre y1 e y2
+                x += tx
+                bordas_esquerda[y] = min(bordas_esquerda[i], int(x))
+                bordas_direita[y] = max(bordas_direita[i], int(x))
+
+    # Pintar pixel a pixel
+    for y in range(y_min, y_max + 1):
+        for x in range(bordas_esquerda[y - y_min], bordas_direita[y - y_min] + 1):
+            # Pintar o pixel (x, y)
+            print(f"Pintando pixel ({x}, {y})")
+
+# Exemplo de uso com um polígono de 4 lados:
+vertices = [(1, 5), (3, 2), (7, 6), (5, 8)]
+
+rasterizar_vertices(vertices)
